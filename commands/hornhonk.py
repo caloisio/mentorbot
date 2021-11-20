964225a3ea60bbddd1a0d1feeea9b12459a1c118
Luke Maxwell <24lbmaxwell@protonmail.com>
2021-11-20 16:49:39 -0500

    horn fixes

--- commands/hornhonk.py
+++ commands/hornhonk.py
@@ -3,7 +3,7 @@ from commands2 import CommandBase
 
 
 class HornHonk(CommandBase):
-    def __init__(self, horn: HornSubsystem) -> None: #amount: int
+    def __init__(self, horn: HornSubsystem) -> None:  #amount: int
         CommandBase.__init__(self)
         #self.repeatAmount = amount
         self.horn = horn
@@ -11,13 +11,9 @@ class HornHonk(CommandBase):
         self.setName(__class__.__name__)
         self.addRequirements([self.horn])
 
-        self.on = False
-
     def execute(self) -> None:
 
         self.horn.horn.set(1.0)
-        self.on = not self.on
 
-    def isFinished(self) -> bool:
+    def end(self, interrupted: bool) -> None:
         self.horn.horn.set(0.0)
-        return (not self.on) #and (self.repeatAmount < 0)
